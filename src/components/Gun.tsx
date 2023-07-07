12525dc65ca9b2fa552ab36140a7ed1a4ac7820f
DoctorTone <drtonesoftware@gmail.com>
2023-07-07 01:53:29 +0100

    Add flash effect on firing

--- src/components/Gun.tsx
+++ src/components/Gun.tsx
@@ -14,9 +14,15 @@ const Gun = () => {
   const blasterRef = useRef<THREE.Group>(null!);
   const player = useXR((state) => state.player);
   const [fireSound] = useState(() => new Audio("./sounds/blaster.ogg"));
+  const [blastIntensity, setIntensity] = useState(0);
+  const [blasting, setBlasting] = useState(false);
 
   const onFire = () => {
     fireSound.play();
+    setBlasting(true);
+    setTimeout(() => {
+      setBlasting(false);
+    }, 200);
   };
 
   useXREvent("selectstart", onFire);
@@ -50,26 +56,33 @@ const Gun = () => {
     blasterRef.current.rotation.copy(controller.rotation);
   });
   return (
-    <group ref={blasterRef} dispose={null}>
-      <group scale={0.5} rotation={[-Math.PI / 3, 0, 0]}>
-        <mesh
-          geometry={nodes.Mesh_blasterA.geometry}
-          material={materials.metal}
-        />
-        <mesh
-          geometry={nodes.Mesh_blasterA_1.geometry}
-          material={materials.dark}
-        />
-        <mesh
-          geometry={nodes.Mesh_blasterA_2.geometry}
-          material={materials.darkMetal}
-        />
-        <mesh
-          geometry={nodes.Mesh_blasterA_3.geometry}
-          material={materials.purple}
-        />
+    <>
+      <group ref={blasterRef} dispose={null}>
+        <group scale={0.5} rotation={[-Math.PI / 3, 0, 0]}>
+          <mesh
+            geometry={nodes.Mesh_blasterA.geometry}
+            material={materials.metal}
+          />
+          <mesh
+            geometry={nodes.Mesh_blasterA_1.geometry}
+            material={materials.dark}
+          />
+          <mesh
+            geometry={nodes.Mesh_blasterA_2.geometry}
+            material={materials.darkMetal}
+          />
+          <mesh
+            geometry={nodes.Mesh_blasterA_3.geometry}
+            material={materials.purple}
+          />
+        </group>
+        {/* <mesh visible={blasting} position={[-0.07, -0.3, -0.225]}>
+          <sphereGeometry args={[0.05, 16, 16]} />
+          <meshLambertMaterial emissive="white" />
+        </mesh> */}
+        <pointLight visible={blasting} position={[-0.07, -0.3, -0.225]} />
       </group>
-    </group>
+    </>
   );
 };
 useGLTF.preload("./models/blaster.gltf");
